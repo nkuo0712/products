b6ff6c2008ca3256a109055d096d25530ae60724
Nana Kuo <nkuo0712@gmail.com>
2021-03-14 14:22:13 +0900

    refactio

--- products.py
+++ products.py
@@ -1,32 +1,47 @@
 import os # operating system
 
 # 讀取檔案
-products = []
-if os.path.isfile('products.csv'): # 檢查檔案在不在
-	with open('products.csv', 'r', encoding='utf-8') as f:
+def read_file(filename):
+	products = []
+	with open(filename, 'r', encoding='utf-8') as f:
 		for line in f:
 			if '商品,價格' in line:
 				continue #繼續
 			name, price = line.strip().split(',')
 			products.append([name, price])
-		print('已讀取以前資料')
-else:
-	print('尚未建立清單')
+	return products
 
 # 讓使用者輸入
-while True:
-	name = input('請輸入商品名稱：')
-	if name == 'q':
-		break
-	price = input('請輸入商品價格：')
-	products.append([name, price])
+def user_input(products):
+	while True:
+		name = input('請輸入商品名稱：')
+		if name == 'q':
+			break
+		price = input('請輸入商品價格：')
+		products.append([name, price])
+	return products
 
 # 印出所有購買紀錄
-for p in products:
-	print(p[0], '的價格是', p[1])
+def print_products(products):
+	for p in products:
+		print(p[0], '的價格是', p[1])
 
 # 寫入檔案
-with open('products.csv', 'w', encoding='utf-8') as f: #檔名.寫入模式.編碼
-	f.write('商品,價格\n')
-	for p in products:
-		f.write(p[0] + ',' + p[1] + '\n')
+def write_file(filename, products):
+	with open(filename, 'w', encoding='utf-8') as f: #檔名.寫入模式.編碼
+		f.write('商品,價格\n')
+		for p in products:
+			f.write(p[0] + ',' + p[1] + '\n')
+
+def main(filename):
+	if os.path.isfile(filename): # 檢查檔案在不在
+		products = read_file(filename)
+		print('已讀取以前資料')
+	else:
+		print('尚未建立清單')
+
+	products = user_input(products)
+	print_products(products)
+	write_file('products.csv', products)
+
+main('products.csv')
